1538d7089f364865ed5fec7b41c8bec90aadc3f3
horv <horvathrobby@gmail.com>
2021-02-22 14:40:57 -0500

    Add i3blocks scripts

--- update.sh
+++ update.sh
@@ -5,5 +5,5 @@ cp ~/.vimrc ~/Documents/Projects/dotfiles/vim/vimrc
 cp ~/.config/alacritty/alacritty.yml ~/Documents/Projects/dotfiles/alacritty/alacritty.yml
 
 cp ~/.config/i3/config ~/Documents/Projects/dotfiles/i3/config
-cp ~/.config/i3/i3blocks.conf ~/Documents/Projects/dotfiles/i3/config
+cp ~/.config/i3/i3blocks.conf ~/Documents/Projects/dotfiles/i3/i3blocks.conf
 
