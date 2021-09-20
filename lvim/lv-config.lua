65291b6a63284149fb3b8acdc861fd83494ac533
Robby Horvath <horvathrobby@gmail.com>
2021-09-20 10:53:23 -0400

    update

--- lvim/lv-config.lua
+++ lvim/lv-config.lua
@@ -51,7 +51,7 @@ lvim.keys.normal_mode = {
 
 -- TODO: User Config for predefined plugins
 -- After changing plugin config exit and reopen LunarVim, Run :PackerInstall :PackerCompile
-lvim.builtin.galaxyline.active = true
+lvim.builtin.galaxyline.active = false
 lvim.builtin.dashboard.active = true
 lvim.builtin.terminal.active = true
 lvim.builtin.gitsigns.active = false
