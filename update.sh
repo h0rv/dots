1f439e6388b592b911fa12be3852a6492c1e0cf0
Robby Horvath <rhorb@proton.me>
2022-11-03 18:48:02 -0400

    Update dots

--- update.sh
+++ update.sh
@@ -9,7 +9,7 @@ cp ~/.config/alacritty/alacritty.yml $wd/alacritty/alacritty.yml
 cp ~/.config/kitty/kitty.conf $wd/kitty/kitty.conf
 
 cp ~/.config/i3/config $wd/i3/config
-cp ~/.config/i3/i3blocks.conf $wd/i3/i3blocks.conf
+cp ~/.config/i3/scripts/* $wd/i3/scripts/
 
 cp ~/.config/gtk-3.0/settings.ini $wd/gtk/settings.ini
 
