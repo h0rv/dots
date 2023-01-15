2a5be02abdd46748cc02204a892ac28f07204bea
Robby Horvath <robbyhorvath@users.noreply.github.com>
2023-01-15 17:35:06 -0500

    Update

---
+++ nvim/lua/horv/remap.lua
@@ -0,0 +1,30 @@
+local set = vim.keymap.set
+
+set('n', '<leader>e', vim.cmd.Lexplore)
+
+set('n', '<leader>w', vim.cmd.w)
+set('n', '<leader>q', vim.cmd.q)
+
+set('i', 'jk', '<ESC>')
+set('i', 'kj', '<ESC>')
+
+set('v', 'J', ":m '>+1<CR>gv=gv")
+set('v', 'K', ":m '<-2<CR>gv=gv")
+
+set('n', 'J', 'mzJ`z')
+set('n', '<C-d>', '<C-d>zz')
+set('n', '<C-u>', '<C-u>zz')
+set('n', 'n', 'nzzzv')
+set('n', 'N', 'Nzzzv')
+
+set('n', '<leader>y', '\"+y')
+set('v', '<leader>y', '\"+y')
+set('n', '<leader>Y', '\"+Y')
+
+set('n', 'Q', '<nop>')
+
+set('n', '<leader>f', function() vim.lsp.buf.format() end)
+
+set('n', '<leader>s', [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
+
+set('n', '<leader>pm', '<cmd>e ~/.config/nvim/lua/horv/packer.lua<CR>');
