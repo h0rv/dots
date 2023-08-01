f79788edb4342a74ba12cbfdbbf6d5c5de10b6d5
Robby <h0rv@users.noreply.github.com>
2023-08-01 13:16:13 -0400

    Update debug

--- nvim/after/plugin/debug.lua
+++ nvim/after/plugin/debug.lua
@@ -2,10 +2,15 @@ local dap = require("dap")
 local dap_vscode_js = require('dap-vscode-js')
 local dapui = require("dapui")
 local set = vim.keymap.set
+local sign = vim.fn.sign_define
 
 ---
 --- dap
 ---
+
+sign("DapBreakpoint", { text = "●", texthl = "DapBreakpoint", linehl = "", numhl = "" })
+sign("DapBreakpointCondition", { text = "●", texthl = "DapBreakpointCondition", linehl = "", numhl = "" })
+sign("DapLogPoint", { text = "◆", texthl = "DapLogPoint", linehl = "", numhl = "" })
 
 -- Set keymaps to control the debugger
 set('n', '<F5>', require 'dap'.continue)
@@ -14,17 +19,17 @@ set('n', '<F11>', require 'dap'.step_into)
 set('n', '<F12>', require 'dap'.step_out)
 set('n', '<leader>b', require 'dap'.toggle_breakpoint)
 set('n', '<leader>B', function()
-    require 'dap'.set_breakpoint(vim.fn.input('Breakpoint condition: '))
+  require 'dap'.set_breakpoint(vim.fn.input('Breakpoint condition: '))
 end)
 
 dap_vscode_js.setup({
-    -- node_path = "node", -- Path of node executable. Defaults to $NODE_PATH, and then "node"
-    -- debugger_path = "(runtimedir)/site/pack/packer/opt/vscode-js-debug",                                                       -- Path to vscode-js-debug installation.
-    -- debugger_cmd = { "js-debug-adapter" }, -- Command to use to launch the debug server. Takes precedence over `node_path` and `debugger_path`.
-    adapters = { 'chrome', 'pwa-node', 'pwa-chrome', 'pwa-msedge', 'node-terminal', 'pwa-extensionHost', 'node', 'chrome' }, -- which adapters to register in nvim-dap
-    -- log_file_path = "(stdpath cache)/dap_vscode_js.log" -- Path for file logging
-    -- log_file_level = false -- Logging level for output to file. Set to false to disable file logging.
-    -- log_console_level = vim.log.levels.ERROR -- Logging level for output to console. Set to false to disable console output.
+  -- node_path = "node", -- Path of node executable. Defaults to $NODE_PATH, and then "node"
+  -- debugger_path = "(runtimedir)/site/pack/packer/opt/vscode-js-debug",                                                       -- Path to vscode-js-debug installation.
+  -- debugger_cmd = { "js-debug-adapter" }, -- Command to use to launch the debug server. Takes precedence over `node_path` and `debugger_path`.
+  adapters = { 'chrome', 'pwa-node', 'pwa-chrome', 'pwa-msedge', 'node-terminal', 'pwa-extensionHost', 'node', 'chrome' },   -- which adapters to register in nvim-dap
+  -- log_file_path = "(stdpath cache)/dap_vscode_js.log" -- Path for file logging
+  -- log_file_level = false -- Logging level for output to file. Set to false to disable file logging.
+  -- log_console_level = vim.log.levels.ERROR -- Logging level for output to console. Set to false to disable console output.
 })
 
 ---
@@ -34,30 +39,30 @@ dap_vscode_js.setup({
 local js_based_languages = { "typescript", "javascript", "typescriptreact" }
 
 for _, language in ipairs(js_based_languages) do
-    dap.configurations[language] = {
-        {
-            type = "pwa-node",
-            request = "launch",
-            name = "Launch file",
-            program = "${file}",
-            cwd = "${workspaceFolder}",
-        },
-        {
-            type = "pwa-node",
-            request = "attach",
-            name = "Attach",
-            processId = require 'dap.utils'.pick_process,
-            cwd = "${workspaceFolder}",
-        },
-        {
-            type = "pwa-chrome",
-            request = "launch",
-            name = "Start Chrome with \"localhost\"",
-            url = "http://localhost:3000",
-            webRoot = "${workspaceFolder}",
-            userDataDir = "${workspaceFolder}/.vscode/vscode-chrome-debug-userdatadir"
-        }
+  dap.configurations[language] = {
+    {
+      type = "pwa-node",
+      request = "launch",
+      name = "Launch file",
+      program = "${file}",
+      cwd = "${workspaceFolder}",
+    },
+    {
+      type = "pwa-node",
+      request = "attach",
+      name = "Attach",
+      processId = require 'dap.utils'.pick_process,
+      cwd = "${workspaceFolder}",
+    },
+    {
+      type = "pwa-chrome",
+      request = "launch",
+      name = "Start Chrome with \"localhost\"",
+      url = "http://localhost:3000",
+      webRoot = "${workspaceFolder}",
+      userDataDir = "${workspaceFolder}/.vscode/vscode-chrome-debug-userdatadir"
     }
+  }
 end
 
 ---
@@ -67,13 +72,13 @@ end
 dapui.setup()
 
 dap.listeners.after.event_initialized["dapui_config"] = function()
-    dapui.open({})
+  dapui.open({})
 end
 dap.listeners.before.event_terminated["dapui_config"] = function()
-    dapui.close({})
+  dapui.close({})
 end
 dap.listeners.before.event_exited["dapui_config"] = function()
-    dapui.close({})
+  dapui.close({})
 end
 
 set('n', '<leader>ui', require 'dapui'.toggle)
@@ -82,12 +87,22 @@ set('n', '<leader>ui', require 'dapui'.toggle)
 --- Use `.vscode/launch.json`
 ---
 
+dap.adapters.node = {
+  type = 'executable',
+  command = 'node',
+  args = { vim.fn.stdpath('data') .. '/mason/packages/node-debug2-adapter/out/src/nodeDebug.js' }
+}
+dap.adapters.node2 = {
+  type = 'executable',
+  command = 'node',
+  args = { vim.fn.stdpath('data') .. '/mason/packages/node-debug2-adapter/out/src/nodeDebug.js' }
+}
 require('dap.ext.vscode').json_decode = require('json5').parse
 require('dap.ext.vscode').load_launchjs(nil,
-    {
-        ['pwa-node'] = js_based_languages,
-        ['node'] = js_based_languages,
-        ['chrome'] = js_based_languages,
-        ['pwa-chrome'] = js_based_languages
-    }
+  {
+    ['pwa-node'] = js_based_languages,
+    ['node'] = js_based_languages,
+    ['chrome'] = js_based_languages,
+    ['pwa-chrome'] = js_based_languages
+  }
 )
