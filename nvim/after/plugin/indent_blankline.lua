4c09c6da5818775c7c9697307b05659260cabffe
Robby <h0rv@users.noreply.github.com>
2023-10-11 19:00:53 -0400

    Migrate to lazy.nvim

--- nvim/after/plugin/indent_blankline.lua
+++ nvim/after/plugin/indent_blankline.lua
@@ -1,6 +1,7 @@
-require('indent_blankline').setup {
-	char = '┊',
-	show_current_context = true,
-	show_trailing_blankline_indent = false,
-}
+require("ibl").setup()
+-- require('indent_blankline').setup {
+-- 	char = '┊',
+-- 	show_current_context = true,
+-- 	show_trailing_blankline_indent = false,
+-- }
 
