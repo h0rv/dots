a5d9f5d66ebcd1dc83bc0711e9cbd1a88c7baadc
Robby <h0rv@users.noreply.github.com>
2023-10-11 19:16:22 -0400

    Update indent_lines

--- nvim/after/plugin/indent_blankline.lua
+++ nvim/after/plugin/indent_blankline.lua
@@ -1,7 +1,7 @@
-require("ibl").setup()
--- require('indent_blankline').setup {
--- 	char = '┊',
--- 	show_current_context = true,
--- 	show_trailing_blankline_indent = false,
--- }
-
+require('ibl').setup({
+    indent = {
+        char = '|', -- '┊',
+    },
+    -- whitespace = { highlight = { "Whitespace", "NonText" } },
+    -- scope = { exclude = { language = {} } },
+})
