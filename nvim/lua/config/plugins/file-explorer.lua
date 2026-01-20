fd506db25243543b0d9cb0bb3444621cf3cd3f38
Robby <robby@sweetspot.so>
2026-01-20 17:40:40 -0500

    add bento and yazi

--- nvim/lua/config/plugins/file-explorer.lua
+++ nvim/lua/config/plugins/file-explorer.lua
@@ -1,31 +1,46 @@
-require("nvim-tree").setup({
-    view = { width = 34 },
-    renderer = { group_empty = true },
-    filters = {
-        dotfiles = false, -- Show dotfiles (e.g., .venv)
-        custom = { "^\\.git$" },
+-- require("nvim-tree").setup({
+--     view = { width = 34 },
+--     renderer = { group_empty = true },
+--     filters = {
+--         dotfiles = false, -- Show dotfiles (e.g., .venv)
+--         custom = { "^\\.git$" },
+--     },
+--     update_focused_file = {
+--         enable = true,
+--         update_root = false,
+--     },
+--     sync_root_with_cwd = true,
+--     respect_buf_cwd = true,
+--
+--     on_attach = function(bufnr)
+--         local api = require("nvim-tree.api")
+--
+--         local function opts(desc)
+--             return { desc = "nvim-tree: " .. desc, buffer = bufnr, noremap = true, silent = true }
+--         end
+--
+--         -- Open file / expand directory
+--         vim.keymap.set("n", "l", api.node.open.edit, opts("Open"))
+--
+--         -- Optional: keep Vim muscle memory
+--         vim.keymap.set("n", "h", api.node.navigate.parent_close, opts("Close directory"))
+--     end,
+-- })
+--
+-- vim.keymap.set("n", "<leader>e", "<cmd>NvimTreeToggle<CR>", { desc = "Explorer: toggle" })
+-- vim.keymap.set("n", "<leader>o", "<cmd>NvimTreeFindFile<CR>", { desc = "Explorer: reveal file" })
+
+require("yazi").setup({
+    open_for_directories = false,
+    keymaps = {
+        show_help = "<f1>",
     },
-    update_focused_file = {
-        enable = true,
-        update_root = false,
-    },
-    sync_root_with_cwd = true,
-    respect_buf_cwd = true,
-
-    on_attach = function(bufnr)
-        local api = require("nvim-tree.api")
-
-        local function opts(desc)
-            return { desc = "nvim-tree: " .. desc, buffer = bufnr, noremap = true, silent = true }
-        end
-
-        -- Open file / expand directory
-        vim.keymap.set("n", "l", api.node.open.edit, opts("Open"))
-
-        -- Optional: keep Vim muscle memory
-        vim.keymap.set("n", "h", api.node.navigate.parent_close, opts("Close directory"))
-    end,
 })
+-- Yazi: toggle (resume last session by default)
+vim.keymap.set("n", "<leader>e", "<cmd>Yazi toggle<CR>", { desc = "Explorer: yazi (resume/toggle)" })
 
-vim.keymap.set("n", "<leader>e", "<cmd>NvimTreeToggle<CR>", { desc = "Explorer: toggle" })
-vim.keymap.set("n", "<leader>o", "<cmd>NvimTreeFindFile<CR>", { desc = "Explorer: reveal file" })
+-- Optional: open in cwd (fresh)
+vim.keymap.set("n", "<leader>E", "<cmd>Yazi cwd<CR>", { desc = "Explorer: yazi (cwd)" })
+
+-- Optional: open at current file (fresh)
+vim.keymap.set({ "n", "v" }, "<leader>o", "<cmd>Yazi<CR>", { desc = "Explorer: yazi (at file)" })
